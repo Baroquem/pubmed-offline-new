7ec0f7365b3235e6dc97aa105c1c5cef67eaf41b
Matthew Connolly <mjc12@cornell.edu>
2020-03-12 21:53:15 -0400

    Rewrite App as class

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   BrowserRouter,
   Route,
@@ -16,61 +16,80 @@ import Bookbag from './components/bookbag/Bookbag';
 import SearchTips from './components/SearchTips';
 import SearchResultList from './components/SearchResultList';
 
-function App() {
+class App extends Component {
 
-  const renderError = error => {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+      indexFields: ["title","abstract","subjects"],
+    }
+  }
+
+  renderError = error => {
     console.log("ERROR", error);
   }
 
-  const keywordSearchHandler = (search) => {
+  keywordSearchHandler = (search) => {
     console.log("handling search", search)
     // history.push(`/?query="${search.value}"`)
   }
 
-  const mainComponent =
-    <>
-      <header>
-        <div className="navlinks">
-          <div id="bookbag"><Link to="/bookbag">Bookbag</Link></div>
-          <div id="howto"><Link to="/howto">Search tips</Link></div>
-        </div>
-        <h1>CPEP - PubMed search</h1>
-      </header>
-      <SelectedFilters />
-      <DataSearch
-        componentId="query"
-        className="search-bar"
-        dataField={["title","abstract","subjects"]}
-        searchOperators={true}
-        queryFormat="and"
-        placeholder="Search terms..."
-        renderError={renderError}
-        URLParams
-        showClear
-      />
-      <ReactiveList
-        componentId="results"
-        react={{and: ['query']}}
-        pagination={true}
-        URLParams
-      >
-        {({ data, error, loading }) => (
-          <SearchResultList results={data} searchHandler={keywordSearchHandler} />
-        )}
-      </ReactiveList>
-    </>;
+  render() {
+    const mainComponent =
+      <>
+        <header>
+          <div className="navlinks">
+            <div id="bookbag"><Link to="/bookbag">Bookbag</Link></div>
+            <div id="howto"><Link to="/howto">Search tips</Link></div>
+          </div>
+          <h1>CPEP - PubMed search</h1>
+        </header>
+        <SelectedFilters />
+        <DataSearch
+          componentId="query"
+          className="search-bar"
+          dataField={this.state.indexFields}
+          searchOperators={true}
+          queryFormat="and"
+          placeholder="Search terms..."
+          renderError={this.renderError}
+          URLParams
+          showClear
+          value={this.state.value}
+          onChange={(value, triggerQuery, event) => {
+            this.setState(
+              {
+                value,
+              },
+              () => triggerQuery(),
+            );
+          }}
+        />
+        <ReactiveList
+          componentId="results"
+          react={{and: ['query']}}
+          pagination={true}
+          URLParams
+        >
+          {({ data, error, loading }) => (
+            <SearchResultList results={data} searchHandler={this.keywordSearchHandler} />
+          )}
+        </ReactiveList>
+      </>;
 
-  return (
-    <div className="App">
-      <ReactiveBase app="articles" credentials="this doesn't really matter" url="http://localhost:9200">
-        <BrowserRouter>
-          <Route exact path="/" render={() => mainComponent} />
-          <Route path="/bookbag" component={Bookbag} />
-          <Route path="/howto" component={SearchTips} />
-        </BrowserRouter>
-      </ReactiveBase>
-    </div>
-  );
+    return (
+      <div className="App">
+        <ReactiveBase app="articles" credentials="this doesn't really matter" url="http://localhost:9200">
+          <BrowserRouter>
+            <Route exact path="/" render={() => mainComponent} />
+            <Route path="/bookbag" component={Bookbag} />
+            <Route path="/howto" component={SearchTips} />
+          </BrowserRouter>
+        </ReactiveBase>
+      </div>
+    );
+  }
 }
 
 export default App;
